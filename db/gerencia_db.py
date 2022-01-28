b8dfa1c4abe11961810b1007e4b68bc0493fbe55
indivp21 <87029524+indivp21@users.noreply.github.com>
2022-01-28 17:48:06 -0500

    20220128

--- db/gerencia_db.py
+++ db/gerencia_db.py
@@ -10,13 +10,13 @@ class GerInDB(BaseModel):
 database_gerencias = Dict[str, GerInDB]
 database_gerencias = {
 	"Gerencia2": GerInDB(**{"name":"Gerencia Servivio a Cliente",
-							"indigen":75.4,
+							"indigen":67.9,
 							"rango":"XX/XX/2021 al XX/XX/2021"}),
 	"Gerencia1": GerInDB(**{"name":"Gerencia de Operación Bancaria",
-							"indigen":97.7,
+							"indigen":96.6,
 							"rango":"XX/XX/2021 al XX/XX/2021"}),
 	"Gerencia3": GerInDB(**{"name":"Gerencia de Operación Bancaria",
-							"indigen":97.7,
+							"indigen":96.6,
 							"rango":"XX/XX/2021 al XX/XX/2021"}),
 }
 
