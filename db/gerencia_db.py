6157d14cf8096c4d421528d518a39381b3255113
aandresmon29 <71345919+aandresmon29@users.noreply.github.com>
2022-06-10 18:37:01 -0500

    update 20220610

--- db/gerencia_db.py
+++ db/gerencia_db.py
@@ -9,7 +9,7 @@ class GerInDB(BaseModel):
 
 database_gerencias = Dict[str, GerInDB]
 database_gerencias = {
-	"Gerencia2": GerInDB(**{"name":"Gerencia Servicio a Cliente",
+	"Gerencia3": GerInDB(**{"name":"Gerencia Servicio a Cliente",
 							"indigen":70.1,
 							"rango":"27/05/2021 al 02/06/2021"}),
 	"Gerencia2": GerInDB(**{"name":"Gerencia Servivio a Cliente",
