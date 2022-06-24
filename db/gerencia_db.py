f8843b23f36129a22cb1aaa49820d6f47605df0b
indivp21 <87029524+indivp21@users.noreply.github.com>
2022-06-24 16:45:21 -0500

    20220624

--- db/gerencia_db.py
+++ db/gerencia_db.py
@@ -12,12 +12,12 @@ database_gerencias = {
 	"Gerencia3": GerInDB(**{"name":"Gerencia Servicio a Cliente",
 							"indigen":70.1,
 							"rango":"27/05/2021 al 02/06/2021"}),
-	"Gerencia2": GerInDB(**{"name":"Gerencia Servicio a Cliente",
-							"indigen":72.4,
-							"rango":"10/06/2022 al 16/06/2022"}),
+	"Gerencia2": GerInDB(**{"name":"Gerencia Servivio a Cliente",
+							"indigen":72.6,
+							"rango":"17/06/2022 al 23/06/2022"}),
 	"Gerencia1": GerInDB(**{"name":"Gerencia de Operación Bancaria",
 							"indigen":98.0,
-							"rango":"10/06/2022 al 16/06/2022"}),                  
+							"rango":"17/06/2022 al 23/06/2022"}),               
 }
 
 def get_gerencia(gerencia: str):
